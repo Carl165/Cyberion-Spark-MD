7508c90984b0c9169ba5139100b27db48ca095f8
carl165 <167853149+Carl165@users.noreply.github.com>
2024-07-27 01:25:26 +0300

    test.js

--- commandes/test.js
+++ commandes/test.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "repo", reaction: "👍", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "repo", reaction: "💬", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '𝗪𝗛𝗔𝗧"𝗦 𝗨𝗣 𝗠𝗬 𝗚𝙀𝙀 𝗧𝗛𝗜𝗦 𝗜𝗦 𝗜𝗕𝗥𝗔𝗛𝗜𝗠-𝗧𝗘𝗖𝗛  \n\n ' + "𝗙𝗢𝗥𝗞 𝗧𝗛𝗘 𝗥𝗘𝗣𝗢 𝗧𝗢 𝗗𝗘𝗣𝗟𝗢𝗬 𝗬𝗢𝗨𝗥 𝗢𝗪𝗡 𝗕𝗢𝗧 *𝗜𝗕𝗥𝗔𝗛𝗜𝗠-𝗧𝗘𝗖𝗛 𝗥𝗘𝗣𝗢.*";
-    let d = ' https://github.com/ibrahimaitech/IBRAHIM-AI-10.10/tree/main';
+    let z = 'HELLO 👋 CUTIE 😌AM CYBERION SPARK MD \n\n ' + "𝗙𝗢𝗥𝗞 𝗧𝗛𝗘 𝗥𝗘𝗣𝗢 𝗧𝗢 𝗗𝗘𝗣𝗟𝗢𝗬 𝗬𝗢𝗨𝗥 𝗢𝗪𝗡 𝗕𝗢𝗧 *CYBERION-SPARK.*";
+    let d = ' https://github.com/Carl165/Cyberion-Spark-MD/tree/main';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/c66d12099fb7a4f62d70a.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
