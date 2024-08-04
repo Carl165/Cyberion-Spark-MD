5fa0467715d3b1c61177698566c5d32e4c90a8be
carl165 <167853149+Carl165@users.noreply.github.com>
2024-08-04 13:43:49 +0300

    Ibraah.js
    Signed-off-by: carl165 <167853149+Carl165@users.noreply.github.com>

--- commandes/Ibraah.js
+++ commandes/Ibraah.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "Ibraah", reaction: "📷", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "ibraah", reaction: "📷", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = 'CYBERION-HD-PHOTO \n\n ' + "*HD.*";
     let d = ' #KING';
