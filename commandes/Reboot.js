35af7aea32cb4f4e1965946ef602053337879885
carl165 <167853149+Carl165@users.noreply.github.com>
2024-08-10 23:18:13 +0300

    Cyberion-Spark-MD ☢️
    Signed-off-by: carl165 <167853149+Carl165@users.noreply.github.com>

---
+++ commandes/Reboot.js
@@ -0,0 +1,31 @@
+const {zokou}=require("../framework/zokou")
+
+
+
+
+
+
+
+zokou({nomCom:"restart",categorie:"Mods",reaction:"♻️"},async(dest,z,com)=>{
+
+
+  
+const{repondre,ms,dev,superUser}=com;
+
+  if(!superUser)
+  {
+    return repondre("This command is for owner only");
+  }
+
+  const {exec}=require("child_process")
+
+    repondre("Cyberion-Spark-MD Restarting ⏳");
+
+  exec("pm2 restart all");
+  
+
+  
+
+
+
+})
